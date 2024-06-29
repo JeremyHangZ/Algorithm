6eb37e05281f4930d1487692f419dd9b39062972
JeremyHangZ <1153697115@qq.com>
2024-06-29 16:48:15 +1000

    两数之和

---
+++ src/Hash/TwoSum.java
@@ -0,0 +1,27 @@
+package Hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];   // 遍历当前元素，并在map中寻找是否有匹配的key
+            if (map.containsKey(temp)) {
+                res[1] = i;
+                res[0] = map.get(temp);
+                break;
+            }
+            map.put(nums[i], i);    // 如果没找到匹配对，就把访问过的元素和下标加入到map中
+        }
+        return res;
+    }
+}
