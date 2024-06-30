942bdeea628737e8e02347a772a421bfe632c724
JeremyHangZ <1153697115@qq.com>
2024-06-30 13:06:01 +1000

    Update TwoSum_1.java

--- src/Hash/TwoSum_1.java
+++ src/Hash/TwoSum_1.java
@@ -6,7 +6,7 @@ import java.util.Map;
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  */
-public class TwoSum {
+public class TwoSum_1 {
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
         if (nums == null || nums.length == 0) {
